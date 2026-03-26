718a4515fa26eb192b091c12229ddd672173e9d9
arushii09 <arushiii323@gmail.com>
2026-03-27 01:36:43 +0530

    add pdf loading and chunking

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import streamlit as st
 from langchain_chroma import Chroma
-from langchain_classic.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain_groq import ChatGroq
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
@@ -13,3 +13,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 model = ChatGroq(model="llama-3.1-8b-instant")
+
+CHROMA_DIR = "chroma_dir"
+def load_and_index_pdf(pdf_path):
+    loader = PyPDFLoader(pdf_path)
+    documents = loader.load()
+    print(f"Loaded {len(documents)} pages")
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+    chunks=splitter.split_documents(documents)
+    print(f"Split into {len(chunks)} chunks")
+    return chunks
+
+
+if __name__ =="__main__":
+    chunks=load_and_index_pdf("research_paper.pdf")
+    print("Done!")
